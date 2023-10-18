910129ef7c2032a98380e5f449ea53dbae1b0c5e
Iwo <g00391231@atu.ie>
2023-10-18 09:35:11 +0100

    Setup

--- src/main/java/ie/atu/week5blocking/Week5BlockingApplication.java
+++ src/main/java/ie/atu/week5blocking/Week5BlockingApplication.java
@@ -3,6 +3,8 @@ package ie.atu.week5blocking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -11,5 +13,6 @@ public class Week5BlockingApplication {
     public static void main(String[] args) {
         SpringApplication.run(Week5BlockingApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() { return new RestTemplate(); }
 }
